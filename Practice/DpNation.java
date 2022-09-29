d09c92a36bd4f6871802d75308f8a89b5dd6624f
Mo Zakir <mozakir178@gmail.com>
2022-09-29 15:59:06 +0530

    Dp Nation Solution

--- Practice/DpNation.java
+++ Practice/DpNation.java
@@ -1,3 +1,47 @@
+
+
+DpNation - Robbing Houses 1 Ended
+Description
+
+Robbery is pretty common in dp nation.
+
+A robber is planning to rob houses along a street. Each house has a certain amount of gold stashed, the only constraint stopping you from robbing each of them is that adjacent houses have a very smart security system connected andit will automatically alert the police if two adjacent houses were robbed  on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money the roober can rob tonightwithoutgetting caught.
+
+
+Input
+1<=T<=10
+
+1<=N<=1000
+
+1<=Ai<=100
+
+
+Output
+output a single integer , which is the solution to the question.
+
+
+Sample Input 1 
+
+3
+2
+1 100
+3
+2 100 99
+4
+100 1 1 100
+Sample Output 1
+
+100
+101
+200
+
+
+### Solution ###
+
+
+
 import java.util.* ;
 class Main{
     public static void main(String[] args){
